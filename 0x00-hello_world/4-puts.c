17320262c7119d0700cbbf243c9a61e548a13994
Giftmuswane <Giftmuswane@gmail.com>
2022-09-08 17:38:46 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,20 +2,18 @@
 
 /**
 
- * main - Entry point
+* main - entry point
 
- *
+*
 
- * Return: always 0
+* Return: 0 after printing the function
 
- */
+*/
 
 int main(void)
-  
-{
-  
-  puts("\"Programming is like building a multilingual puzzle");
-  
-  return (0);
-  
+
+{puts("\"Programming is like building a multilingual puzzle");
+ 
+ return (0);
+ 
 }
