8bc30f2a321fbda466e8d926de69f063de15c529
Giftmuswane <Giftmuswane@gmail.com>
2022-09-11 00:58:38 -0700

    Hello, printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 /**
-* main - entry point
-*
-* Return: Always 0 (Success)
+* main - prints out a spring via printf
+* Discription: Program that prints out the string s followed by a new line 
+* Return: int 0 when done and no errors
 */
+
 int main(void)
 {
-	printf(with proper grammar, but the outcome is a piece of art,\n");
+	printf("with proper gramma, but the outcome is a piece of art,\n");
 	return (0);
 }
