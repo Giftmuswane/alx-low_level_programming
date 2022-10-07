3788037e05e13d2e923bdb7cfe8da749eb27c203
Gift Muswane <Giftmuswane@gmail.com>
2022-10-07 15:03:21 +0100

    Trust no one

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,20 +2,20 @@
 #include <stdlib.h>
 
 /**
- *  *malloc_checked - allocate memory.
- *   *@b: size of the memory to be allocated.
- *    *Return: pointer;
- *     **/
+*malloc_checked - allocate memory.
+*@b: size of the memory to be allocated.
+*Return: pointer;
+**/
 
 void *malloc_checked(unsigned int b)
 {
-	char *ip;
+char *ip;
 
-	ip = malloc(b);
-	if (ip == NULL)
-	{
-		exit(98);
-		return (NULL);
-	}
-	return (ip);
+ip = malloc(b);
+if (ip == NULL)
+{
+exit(98);
+return (NULL);
+}
+return (ip);
 }
